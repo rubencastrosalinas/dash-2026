cac40122559690e07e9c322fe4bd4bcc75b96abf
rubencastrosalinas <134544644+rubencastrosalinas@users.noreply.github.com>
2026-09-22 23:54:04 -0300

    Update meta-data.js

--- meta-data.js
+++ meta-data.js
@@ -8,3 +8,7 @@ DATA.rows[DATA.rows.length-1]=[36,6,72639,18486,1120,449760,2];
 DATA.campaigns[26]="Tráfico tienda – Cupón Providencia";
 DATA.weeks.push("2026-09-17");
 DATA.rows.push([37,3,49013,20095,886,39990,1],[37,6,27527,8971,555,187990,1],[37,26,774,207,10,0,0]);
+// Semana del 17 de septiembre recalculada con los días 20, 21 y 22, y segunda campaña de tráfico a tienda.
+DATA.campaigns[27]="Tráfico tienda – Martes pila gratis";
+DATA.rows=DATA.rows.filter(r=>r[0]!==37);
+DATA.rows.push([37,3,100672,39492,1733,191750,3],[37,6,66398,21116,1301,187990,1],[37,26,26798,16741,820,0,0],[37,27,10927,43804,127,0,0]);
